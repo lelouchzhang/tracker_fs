d58cc8171dc4496f5b65dff547196d2121de8ec2
lelouchZhang <zhch8128@gmail.com>
2025-09-30 13:52:30 +0800

    Fix: Using Local Fonts Instead Default

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,42 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+const geistSans = localFont({
+  src: [
+    {
+      path: "../public/assets/fonts/Geist-VariableFont_wght.ttf",
+      style: "normal",
+      weight: "400",
+    },
+  ],
+  variable: "--font-geist-sans",
+});
+const geistMono = localFont({
+  src: [
+    {
+      path: "../public/assets/fonts/GeistMono-VariableFont_wght.ttf",
+      style: "normal",
+      weight: "400",
+    },
+  ],
   variable: "--font-geist-mono",
-  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
   title: "Fenmiao Tracker",
-  description: "Track real-time stock prices, get personalized alerts and explore detailed company insights.",
+  description:
+    "Track real-time stock prices, get personalized alerts and explore detailed company insights.",
 };
 
 export default function RootLayout({
---
+++ components/Header.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 header">
+      <div className="container header-wrapper">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.png"
+            width={140}
+            height={32}
+            alt="logo"
+            className="h-8 w-auto cursor-pointer"
+          />
+        </Link>
+        <nav className="hidden sm:block">
+          {/* <NavItems initialStocks={initialStocks} /> */}
+        </nav>
+        {/* <UserDropdown user={user} initialStocks={initialStocks} /> */}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
